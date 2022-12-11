70a7bbb78c0d938a247b49ca30962c433d9b0e00
AlexW850 <alexander.wastl@st.oth-regensburg.de>
2022-12-11 12:09:52 +0000

    Added cubit_bloc project and did cubit implementation

---
+++ cubit_vs_bloc/bin/cubit_start.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'package:cubit_vs_bloc/counter_cubit.dart';
+
+Future<void> main(List<String> arguments) async {
+  print('Program is starting');
+
+  final counterCubit = CounterCubit();
+
+  print('Inital state of cubit: ${counterCubit.state}');
+
+  final listener = counterCubit.stream.listen(print);
+
+  counterCubit.increment();
+  counterCubit.increment();
+  counterCubit.increment();
+  counterCubit.decrement();
+
+  await Future.delayed(
+      Duration.zero); // needed to not cancel the stream subscription right here
+
+  await listener.cancel(); // cancel listener
+
+  await counterCubit.close(); // close cubit
+}
