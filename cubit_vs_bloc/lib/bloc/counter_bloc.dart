e7c049292c760d7bd550f65097ee463790b9a9eb
AlexW850 <alexander.wastl@st.oth-regensburg.de>
2022-12-11 12:43:46 +0000

    Fixed small event call error

--- cubit_vs_bloc/lib/bloc/counter_bloc.dart
+++ cubit_vs_bloc/lib/bloc/counter_bloc.dart
@@ -13,6 +13,6 @@ class DecrementEvent extends CounterEvent {}
 class CounterBloc extends Bloc<CounterEvent, int> {
   CounterBloc() : super(0) {
     on<IncrementEvent>((event, emit) => emit(state + 1));
-    on<IncrementEvent>((event, emit) => emit(state - 1));
+    on<DecrementEvent>((event, emit) => emit(state - 1));
   }
 }
