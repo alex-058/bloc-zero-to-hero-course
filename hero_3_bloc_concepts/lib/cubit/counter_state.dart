c77a09a5cf823ddb0697cdf323bb1bac51e53c2b
AlexW850 <alexander.wastl@st.oth-regensburg.de>
2022-12-15 17:57:52 +0000

    Added counter cubit + fundamental methods (increment, decrement) in cubit + single counterState class which holds int value

---
+++ hero_3_bloc_concepts/lib/cubit/counter_state.dart
@@ -0,0 +1,10 @@
+part of 'counter_cubit.dart';
+
+@immutable
+class CounterState {
+  // int value is the state in this example
+  int counterValue;
+
+  // required parameter makes it named parameter
+  CounterState({required this.counterValue});
+}
