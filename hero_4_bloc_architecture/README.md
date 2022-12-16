a997c6160ebd4fad5c408193be627883a1de14ec
AlexW850 <alexander.wastl@st.oth-regensburg.de>
2022-12-16 11:48:04 +0000

    Created new project for #3 bloc architecture course

---
+++ hero_4_bloc_architecture/README.md
@@ -0,0 +1,5 @@
+# hero_4_bloc_architecture
+
+#4 BLoC Architecture course 
+
+## Introduction
---
+++ hero_4_bloc_architecture/android/app/src/main/kotlin/com/example/hero_4_bloc_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hero_4_bloc_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
