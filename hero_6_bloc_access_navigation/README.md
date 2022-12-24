16972c9aa839162637b4a6a36906079908d21c4f
AlexW850 <alexander.wastl@st.oth-regensburg.de>
2022-12-24 13:11:12 +0100

    Altered structure (presentation, logic) of application, added second screen, implemented first named routing (results in an error)

--- hero_6_bloc_access_navigation/README.md
+++ hero_6_bloc_access_navigation/README.md
@@ -7,3 +7,16 @@ A new Flutter project that deals with the content for access and navigation
 ![](2022-12-24-12-37-17.png)
 
 Therefore this is called local access, because only one screen (BuildContext) has access to that cubit and the provided functions that alter the state 
+
+## Routing options and route access
+
+![](2022-12-24-13-09-55.png)
+
+![](2022-12-24-13-06-29.png) 
+
+## Important to know 
+
+The home property in the Material App is pushed first in the navigation stack (default route)
+
+"The widget for the default route of the app ([Navigator.defaultRouteName], which is /).
+This is the route that is displayed first when the application is started normally, unless [initialRoute] is specified. It's also the route that's displayed if the [initialRoute] can't be displayed."
--- hero_6_bloc_access_navigation/lib/main.dart
+++ hero_6_bloc_access_navigation/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'cubit/counter_cubit.dart';
+import 'package:hero_6_bloc_access_navigation/presentation/screens/home_screen.dart';
+import 'package:hero_6_bloc_access_navigation/presentation/screens/second_screen.dart';
+import 'logic/cubit/counter_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => BlocProvider<CounterCubit>(
+              create: (context) => CounterCubit(),
+              child: const HomeScreen(
+                title: 'Flutter Demo Home Page',
+                color: Colors.blueAccent,
+              ),
+            ),
+        'second_screen': (context) => const SecondScreen(
+              title: 'Second Screen',
+              color: Colors.redAccent,
+            ),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -21,103 +36,9 @@ class MyApp extends StatelessWidget {
       // Providing single instance of counter cubit at root of widget tree (dependency injection)
       home: BlocProvider<CounterCubit>(
         create: (context) => CounterCubit(),
-        child: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                if (state.wasIncremented == true) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Incremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  );
-                } else if (state.wasIncremented == false) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Decremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  );
-                  ;
-                }
-              },
-              builder: (context, state) {
-                if (state.counterValue < 0) {
-                  return Text(
-                    'BRR, NEGATIVE ${state.counterValue}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else if (state.counterValue % 2 == 0) {
-                  return Text(
-                    'YAAAY ${state.counterValue}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else if (state.counterValue == 5) {
-                  return Text(
-                    'HMM, NUMBER 5',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else {
-                  return Text(
-                    state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                }
-              },
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  heroTag: Text('${widget.title}'),
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                  },
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  heroTag: Text('${widget.title} #2'),
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            ),
-          ],
+        child: const HomeScreen(
+          title: 'Flutter Demo Home Page',
+          color: Colors.blueAccent,
         ),
       ),
     );
